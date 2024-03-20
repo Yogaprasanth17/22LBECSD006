c966e760053ace0d37269c7a8dd7891d52e60604
yogaprasanth9642 <yogivelu17@gmail.com>
2024-03-20 12:32:38 +0530

    project added

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <table>
+          <tr>
+            <td><input class = "text" type="text" id="ans"></input></td>
+              <td><input class = "but" type="button" value="c"></input></td>
+            </tr>
+            <tr>
+              <td><input class = "but" type="button" value="1"></input></td>
+              <td><input class = "but" type="button" value="2"></input></td>
+              <td><input class = "but" type="button" value="3"></input></td>
+              <td><input  class = "but" type="button" value="%"></input></td>
+            </tr>
+            <tr>
+              <td><input class = "but" type="button" value="4"></input></td>
+              <td><input class = "but" type="button" value="5"></input></td>
+              <td><input class = "but" type="button" value="6"></input></td>
+              <td><input  class = "but" type="button" value="/"></input></td>
+            </tr>
+            <tr>
+              <td><input  class = "but" type="button" value="7"></input></td>
+              <td><input class = "but" type="button" value="8"></input></td>
+              <td><input class = "but" type="button" value="9"></input></td>
+              <td><input class = "but" type="button" value="*"></input></td>
+            </tr>
+            <tr>
+              <td><input class = "but" type="button" value="0"></input></td>
+              <td><input class = "but" type="button" value="."></input></td>
+              <td><input class = "but" type="button" value="="></input></td>
+              <td><input class = "but" type="button" value="+"></input></td>
+            </tr>
+        </table>
+      </header>
+    </div>
+  );
+}
+
+export default App;
